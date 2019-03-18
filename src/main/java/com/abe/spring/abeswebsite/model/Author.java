9e13705fddd9734ea8515dd0bb3f16df177892b7
Brendan Cunnie <brendan.cunnie@showtime.net>
2019-03-18 07:44:05 -0400

    Bootstrap data.  Fix Author toString.  Add Spring datasource.

--- src/main/java/com/abe/spring/abeswebsite/model/Author.java
+++ src/main/java/com/abe/spring/abeswebsite/model/Author.java
@@ -79,6 +79,6 @@ public class Author {
 
   @Override
   public String toString(){
-    return "Author{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", books=" + books + '}';
+    return "Author{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' +  '}';
   }
 }
